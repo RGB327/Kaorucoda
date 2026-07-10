dc75ec1e00c794cf7debf23be3cb2d871b14a175
RGB327 <semi885522@gmail.com>
2026-07-10 14:55:10 +0900

    Fix dash key input reverted to legacy Input API by develop merge
    The "대쉬 키 스페이스로 바꿈" change from develop used the old
    UnityEngine.Input class, which throws under this project's
    Input System Package-only setting. Switched to Keyboard.current.

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -31,7 +31,7 @@ public class Movement : MonoBehaviour
                  - (Keyboard.current.sKey.isPressed || Keyboard.current.downArrowKey.isPressed ? 1f : 0f);
         inputVec = new Vector2(x, y);
 
-        if (canDash && Input.GetKeyDown(KeyCode.Space))
+        if (canDash && Keyboard.current.spaceKey.wasPressedThisFrame)
         {
             canDash = false;
             isDash = true;
